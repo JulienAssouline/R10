ceb8ca898e37cb08cec6a9ca63cccf3a307757c1
JulienAssouline <julien1993@hotmail.ca>
2019-05-31 09:51:05 -0400

    added map

--- MapLocation.js
+++ MapLocation.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {styles} from "./styles"
+import MapView from 'react-native-maps'
+
+// export default class App extends Component<Props> {
+//   render() {
+//     return (
+//       <MapView style={{flex: 1}}
+//       region={{
+//         latitude: 42.882004,
+//         longitude: 74.582748,
+//         latitudeDelta: 0.0922,
+//         longitudeDelta: 0.0421
+//         }} showsUserLocation={true}
+//       />
+//       );
+//   }}
+
 
 const MapLocation = () => {
   return (
-   <View style={styles.container}>
-      <Text>Map</Text>
-    </View>
+      <MapView style={{flex: 1}}
+      region={{
+        latitude: 43.6437903,
+        longitude: -79.3978698,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+        }} showsUserLocation={true}
+      />
     )
 }
 
